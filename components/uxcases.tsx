245eec0cbbae443af595d3d246937cab6d07e34c
EduardoGomezPueyo <tobacor9796@gmail.com>
2021-10-13 12:21:55 +0200

    Added rounded borders to UX images and focus ring

--- components/uxcases.tsx
+++ components/uxcases.tsx
@@ -11,16 +11,16 @@ const UxCases = () => {
             <Section.Title className="">Ux Design Cases</Section.Title>
             <Section.Subtitle>A bit in depth in the Ux-Design process</Section.Subtitle>
             <div className="grid grid-cols-1 lg:grid-cols-2 w-full lg:w-[80%] mx-auto gap-y-8 lg:gap-4">
-                <Link href="/uxcases/sakura-sushi">
+                <Link href="/uxcases/sakura-sushi" className="overflow-hidden rounded-xl focus:ring-pink-600 hover:ring-2 hover:ring-pink-600">
                     <Image src={SushiThumbnail} placeholder="blur" layout="responsive" alt="Sushi Booking App Image" />
                 </Link>
-                <Link href="/">
+                <Link href="/" className="overflow-hidden rounded-xl focus:ring-blue-500 hover:ring-2 hover:ring-blue-500">
                     <Image src={comingSoon} placeholder="blur" layout="responsive" alt="" />
                 </Link>
-                <Link href="/">
+                <Link href="/" className="overflow-hidden rounded-xl focus:ring-blue-500 hover:ring-2 hover:ring-blue-500">
                     <Image src={comingSoon} placeholder="blur" layout="responsive" alt="" />
                 </Link>
-                <Link href="/">
+                <Link href="/" className="overflow-hidden rounded-xl focus:ring-blue-500 hover:ring-2 hover:ring-blue-500">
                     <Image src={comingSoon} placeholder="blur" layout="responsive" alt="" />
                 </Link>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,7 @@ module.exports = {
         },
 
         extend: {
+            ringWidth: ["hover"],
             gridTemplateColumns: {
                 // Complex site-specific column configuration
                 projects: "800px 800px",
