c2ccbaeaaf92208a691d448e36f698f50906d478
Eduardo Gómez Pueyo <eduardogomezpueyo@gmail.com>
2026-09-24 19:29:05 +0200

    Create Case studies page, & fix videos not playing

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,7 +1,8 @@
 export const ui = {
   en: {
     // Nav
-    'nav.work': 'Work',
+    'nav.caseStudies': 'Case Studies',
+    'nav.showcase': 'Showcase',
     'nav.about': 'About',
     // Hero
     'hero.greeting': 'Eduardo Gómez',
@@ -10,7 +11,9 @@ export const ui = {
     // Home
     'home.selectedWork': 'Selected Work',
     'home.viewProject': 'View project',
-    'home.notableWork': 'Work',
+    'home.notableWork': 'Showcase',
+    'home.caseStudies': 'Case Studies',
+    'home.viewCaseStudy': 'View case study',
     // About
     'about.title': 'Nice to meet you',
     'about.bio': "Yup! That's me, on the right, during my birthday, being yelled at because I did not put the cake facing the camera taking photos.\n\n Hi, I'm Eduardo! I was a journalist that decided to change paths and became a UX designer. I've done a lot of stuff, a little bit of this and that, and ended up knowing how to code a little bit as well.\n\nI really like problem solving in design, especially web/mobile sites. I’ve worked on stack teams with so much talent and I hope to continue doing so because it’s the best way to keep learning and moving forward.\n\nWhen I'm not working you can find me playing with my cat, playing PC games with my friends or searching for a new keycap set.",
@@ -41,7 +44,8 @@ export const ui = {
   },
   es: {
     // Nav
-    'nav.work': 'Trabajo',
+    'nav.caseStudies': 'Casos de estudio',
+    'nav.showcase': 'Showcase',
     'nav.about': 'Sobre mí',
     // Hero
     'hero.greeting': 'Eduardo Gómez',
@@ -50,7 +54,9 @@ export const ui = {
     // Home
     'home.selectedWork': 'Trabajo seleccionado',
     'home.viewProject': 'Ver proyecto',
-    'home.notableWork': 'Trabajo',
+    'home.notableWork': 'Showcase',
+    'home.caseStudies': 'Casos de estudio',
+    'home.viewCaseStudy': 'Ver caso de estudio',
     // About
     'about.title': 'Encantado de verte',
     'about.bio': "Efectivamente, ese soy yo, a la derecha, durante mi cumpleaños, recibiendo gritos porque no puse la tarta hacia la cámara para la foto.\n\n¡Hola, soy Eduardo! Estudié periodismo y decidí cambiar de camino para convertirme en diseñador UX. He hecho de todo un poco y acabé aprendiendo también algo de programación.\n\nMe encanta resolver problemas de diseño, especialmente en sitios web y móviles. He trabajado en equipos con muchísimo talento y espero seguir haciéndolo porque es la mejor forma de seguir aprendiendo. Cuando no estoy trabajando puedes encontrarme jugando con mi gata, jugando en el PC con mis amigos o buscando un nuevo set de keycaps.",
